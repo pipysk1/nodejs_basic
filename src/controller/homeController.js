f88d98be39d5ec5126afcdd1624eb9c106f9804a
Mai mai <lvmai1801@gmail.com>
2022-07-25 17:29:40 +0700

    upload file

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,44 +1,97 @@
 import pool from "../configs/connectDB";
+import multer from "multer";
 
 let getHomePage = async (req, res) => {
-    const [rows, fields] = await pool.execute("SELECT * FROM `users` ");
-    return res.render("index.ejs", {dataUser: rows, test: "abc String "});
+  const [rows, fields] = await pool.execute("SELECT * FROM `users` ");
+  return res.render("index.ejs", { dataUser: rows, test: "abc String " });
 };
 
 let getDetailPage = async (req, res) => {
-    let userId = req.params.userId;
-    let [user] = await pool.execute(`select * from users where id =?`, [userId]);
-    console.log(user);
-    return res.send(JSON.stringify(user));
+  let userId = req.params.userId;
+  let [user] = await pool.execute(`select * from users where id =?`, [userId]);
+  console.log(user);
+  return res.send(JSON.stringify(user));
 };
 
 let createNewUser = async (req, res) => {
-    console.log(">>>check req, ", req.body);
-    let {firstName, lastName, email, adress} = req.body;
-    // (????) điền giá trị
-    await pool.execute(`insert into users(firstName, lastName, email, adress)  values  (?,?,?,?)`, [firstName, lastName, email, adress]);
-    return res.redirect("/");
+  console.log(">>>check req, ", req.body);
+  let { firstName, lastName, email, adress } = req.body;
+  // (????) điền giá trị
+  await pool.execute(
+    `insert into users(firstName, lastName, email, adress)  values  (?,?,?,?)`,
+    [firstName, lastName, email, adress]
+  );
+  return res.redirect("/");
 };
 
 let deleteUser = async (req, res) => {
-    let userId = req.body.userId;
-    await pool.execute(`delete from users where id = ? `, [userId]);
-    return res.redirect("/");
+  let userId = req.body.userId;
+  await pool.execute(`delete from users where id = ? `, [userId]);
+  return res.redirect("/");
 };
 
 let getEditPage = async (req, res) => {
-    let id = req.params.id;
-    let [user] = await pool.execute(`select * from users where id = ?`, [id]);
-    console.log(user)
-    return res.render("updates.ejs", {dataUser: user[0]});
+  let id = req.params.id;
+  let [user] = await pool.execute(`select * from users where id = ?`, [id]);
+  console.log(user);
+  return res.render("updates.ejs", { dataUser: user[0] });
 };
 
 let postUpdateUser = async (req, res) => {
-    console.log('check req, ', req.body);
-    let {firstName, lastName, email, adress, id} = req.body;
-    await pool.execute(`UPDATE users SET firstName = ?, lastName = ?, email = ?, adress = ? WHERE id = ?`, [firstName, lastName, email, adress, id]);
-    return res.redirect("/");
-}
+  console.log("check req, ", req.body);
+  let { firstName, lastName, email, adress, id } = req.body;
+  await pool.execute(
+    `UPDATE users SET firstName = ?, lastName = ?, email = ?, adress = ? WHERE id = ?`,
+    [firstName, lastName, email, adress, id]
+  );
+  return res.redirect("/");
+};
+
+let getUploadFilePage = async (req, res) => {
+  return res.render("uploadFile.ejs");
+};
+
+let handleUploadFile = async (req, res) => {
+  // 'profile_pic' is the name of our file input field in the HTML form
+
+  if (req.fileValidationError) {
+    return res.send(req.fileValidationError);
+  } else if (!req.file) {
+    return res.send("Please select an image to upload");
+  }
+
+  res.send(
+    `You have uploaded this image: <hr/><img src="/image/${req.file.filename}" width="500"><hr /><a href="/upload">Upload another image</a>`
+  );
+};
+
+let handleUploadMutipleFiles = async (req, res) => {
+  if (req.fileValidationError) {
+    return res.send(req.fileValidationError);
+  } else if (!req.files) {
+    return res.send("Please select an image to upload");
+  }
+
+  let result = "You have uploaded these images: <hr />";
+  const files = req.files;
+  let index, len;
+
+  // Loop through all the uploaded images and display them on frontend
+  for (index = 0, len = files.length; index < len; ++index) {
+    result += `<img src="/image/${files[index].filename} "width="300" style="margin-right: 20px;">`;
+  }
+  result += '<hr/><a href="/upload">Upload more images</a>';
+  res.send(result);
+};
+
 module.exports = {
-    getHomePage, getDetailPage, createNewUser, deleteUser, getEditPage, postUpdateUser,
+  getHomePage,
+  getDetailPage,
+  createNewUser,
+  deleteUser,
+  getEditPage,
+  postUpdateUser,
+  getUploadFilePage,
+  handleUploadFile,
+  handleUploadMutipleFiles,
 };
--- src/route/web.js
+++ src/route/web.js
@@ -1,7 +1,35 @@
 import express from "express";
+import multer from "multer";
+import path from "path";
+var appRoot = require("app-root-path");
 import homeController from "../controller/homeController";
 
 let route = express.Router();
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, appRoot + "/src/public/image/");
+  },
+
+  // By default, multer removes file extensions so let's add them back
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+const imageFilter = function (req, file, cb) {
+  // Accept images only
+  if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+    req.fileValidationError = "Only image files are allowed!";
+    return cb(new Error("Only image files are allowed!"), false);
+  }
+  cb(null, true);
+};
+
+let upload = multer({ storage: storage, fileFilter: imageFilter });
 
 const initWebRoute = (app) => {
   route.get("/", homeController.getHomePage);
@@ -10,10 +38,18 @@ const initWebRoute = (app) => {
   route.post("/delete-user", homeController.deleteUser);
   route.get("/getEdit-user/:id", homeController.getEditPage);
   // route.post("/update-users", homeController.postUpdateUser);
-  route.post('/updateUser',homeController.postUpdateUser);
-
-
-
+  route.post("/updateUser", homeController.postUpdateUser);
+  route.get("/upload", homeController.getUploadFilePage);
+  route.post(
+    "/upload-profile-pic",
+    upload.single("profile_pic"),
+    homeController.handleUploadFile
+  );
+  route.post(
+    "/upload-multiple-images",
+    upload.array("multiple_images", 3),
+    homeController.handleUploadMutipleFiles
+  );
   route.get("/about", (req, res) => {
     res.send("index.ejs");
   });
